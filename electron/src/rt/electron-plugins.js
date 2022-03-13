a38c61c92575e6565386c3b917f3a3bb5c83dc2a
jepiqueau <jepi.queau@free.fr>
2022-03-13 14:25:00 +0100

    update to @capacitor/core@3.4.3

--- electron/src/rt/electron-plugins.js
+++ electron/src/rt/electron-plugins.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const CapacitorDataStorageSqlite = require('/Users/queaujeanpierre/Development/latest/angular-data-storage-sqlite-app-starter/electron/node_modules/capacitor-data-storage-sqlite/electron/dist/plugin.js')
+const CapacitorDataStorageSqlite = require('../../../node_modules/capacitor-data-storage-sqlite/electron/dist/plugin.js');
 
 module.exports = {
   CapacitorDataStorageSqlite,
